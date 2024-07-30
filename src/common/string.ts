34b7a19b99742de5e196338b9c9d3d4eeec73798
GHBJayce <jaycedeng@outlook.com>
2024-07-30 16:39:42 +0800

    keep try

--- src/common/string.ts
+++ src/common/string.ts
@@ -13,10 +13,12 @@ const trimCommon = (needle?: string): string => {
  * @param {String} needle 默认空白符，可指定要去除字符，多个字符使用英文逗号分隔
  * @returns {String}
  */
-String.prototype.clueTrim = function (needle?: string): string {
-  const core: string = trimCommon(needle)
-  const rule: string = `^([${core}]*)|([${core}]*)$`
-  return this.replace(new RegExp(rule, 'g'), '')
+if (!String.prototype.hasOwnProperty('clueTrim')) {
+  String.prototype.clueTrim = function (needle?: string): string {
+    const core: string = trimCommon(needle)
+    const rule: string = `^([${core}]*)|([${core}]*)$`
+    return this.replace(new RegExp(rule, 'g'), '')
+  }
 }
 
 /**
@@ -24,10 +26,12 @@ String.prototype.clueTrim = function (needle?: string): string {
  * @param {String} needle 默认空白符，可指定要去除字符，多个字符使用英文逗号分隔
  * @returns {String}
  */
-String.prototype.clueTrimLeft = function (needle?: string): string {
-  const core: string = trimCommon(needle)
-  const rule: string = `^([${core}]*)`
-  return this.replace(new RegExp(rule), '')
+if (!String.prototype.hasOwnProperty('clueTrimLeft')) {
+  String.prototype.clueTrimLeft = function (needle?: string): string {
+    const core: string = trimCommon(needle)
+    const rule: string = `^([${core}]*)`
+    return this.replace(new RegExp(rule), '')
+  }
 }
 
 /**
@@ -35,26 +39,32 @@ String.prototype.clueTrimLeft = function (needle?: string): string {
  * @param {String} needle 默认空白符，可指定要去除字符，多个字符使用英文逗号分隔
  * @returns {String}
  */
-String.prototype.clueTrimRight = function (needle?: string): string {
-  const core: string = trimCommon(needle)
-  const rule: string = `([${core}]*)$`
-  return this.replace(new RegExp(rule, 'g'), '')
+if (!String.prototype.hasOwnProperty('clueTrimRight')) {
+  String.prototype.clueTrimRight = function (needle?: string): string {
+    const core: string = trimCommon(needle)
+    const rule: string = `([${core}]*)$`
+    return this.replace(new RegExp(rule, 'g'), '')
+  }
 }
 
 /**
  * 转化为小写
  * @returns {String}
  */
-String.prototype.toLower = function (): string {
-  return this.toLocaleLowerCase()
+if (!String.prototype.hasOwnProperty('toLower')) {
+  String.prototype.toLower = function (): string {
+    return this.toLocaleLowerCase()
+  }
 }
 
 /**
  * 转化为大写
  * @returns {String}
  */
-String.prototype.toUpper = function (): string {
-  return this.toLocaleUpperCase()
+if (!String.prototype.hasOwnProperty('toUpper')) {
+  String.prototype.toUpper = function (): string {
+    return this.toLocaleUpperCase()
+  }
 }
 
 /**
@@ -63,10 +73,12 @@ String.prototype.toUpper = function (): string {
  * @param {Number} index 插入的索引位置
  * @returns {String}
  */
-String.prototype.insert = function (needle: string, index: number): string {
-  const str1: string = this.substring(0, index)
-  const str2: string = this.substring(index)
-  return str1 + needle + str2
+if (!String.prototype.hasOwnProperty('insert')) {
+  String.prototype.insert = function (needle: string, index: number): string {
+    const str1: string = this.substring(0, index)
+    const str2: string = this.substring(index)
+    return str1 + needle + str2
+  }
 }
 
 /**
@@ -75,10 +87,12 @@ String.prototype.insert = function (needle: string, index: number): string {
  * @param {Number} index 被替换值的位置
  * @returns {String}
  */
-String.prototype.indexReplace = function (needle: string, index: number): string {
-  const strArr: string[] = this.split('')
-  strArr.splice(index, 1, needle)
-  return strArr.join('')
+if (!String.prototype.hasOwnProperty('indexReplace')) {
+  String.prototype.indexReplace = function (needle: string, index: number): string {
+    const strArr: string[] = this.split('')
+    strArr.splice(index, 1, needle)
+    return strArr.join('')
+  }
 }
 
 /**
@@ -87,26 +101,28 @@ String.prototype.indexReplace = function (needle: string, index: number): string
  * @param {Number} startIndex 开始替换的索引位置
  * @param {Number} endIndex 结束替换的索引位置
  */
-String.prototype.rangeIndexReplace = function (
-  needle: string,
-  startIndex?: number,
-  endIndex?: number
-): string {
-  const length: number = this.length
-  if (startIndex === undefined) {
-    return this.toString()
-  }
-  endIndex = endIndex || length
-  if (!isSet(startIndex) || endIndex < startIndex) {
-    return this.toString()
-  }
+if (!String.prototype.hasOwnProperty('rangeIndexReplace')) {
+  String.prototype.rangeIndexReplace = function (
+    needle: string,
+    startIndex?: number,
+    endIndex?: number
+  ): string {
+    const length: number = this.length
+    if (startIndex === undefined) {
+      return this.toString()
+    }
+    endIndex = endIndex || length
+    if (!isSet(startIndex) || endIndex < startIndex) {
+      return this.toString()
+    }
 
-  endIndex = endIndex >= length ? length : endIndex
-  startIndex = startIndex >= length ? length : startIndex
+    endIndex = endIndex >= length ? length : endIndex
+    startIndex = startIndex >= length ? length : startIndex
 
-  const strBegin: string = this.substring(0, startIndex)
-  const strEnd: string = this.substring(endIndex + 1, this.length)
-  return strBegin + needle + strEnd
+    const strBegin: string = this.substring(0, startIndex)
+    const strEnd: string = this.substring(endIndex + 1, this.length)
+    return strBegin + needle + strEnd
+  }
 }
 
 /**
@@ -115,29 +131,33 @@ String.prototype.rangeIndexReplace = function (
  * @param {String} needle
  * @returns {Number}
  */
-String.prototype.getShowCount = function (needle: string): number {
-  return this.split(needle).length - 1
+if (!String.prototype.hasOwnProperty('getShowCount')) {
+  String.prototype.getShowCount = function (needle: string): number {
+    return this.split(needle).length - 1
+  }
 }
 
 /**
  * 统计字符串中的字符出现次数
  * @returns {Object}
  */
-String.prototype.getShowCounts = function (): object {
-  let result: object = {}
-  if (this && !isEmpty(this)) {
-    let matchRes: RegExpMatchArray | null = this.split('')
-      .sort()
-      .join('')
-      .match(/(.)\1*/g)
-    if (matchRes) {
-      return matchRes.reduce((res: ObjectAny, cur: string) => {
-        res[cur[0]] = cur.length
-        return res
-      }, {})
+if (!String.prototype.hasOwnProperty('getShowCounts')) {
+  String.prototype.getShowCounts = function (): object {
+    let result: object = {}
+    if (this && !isEmpty(this)) {
+      let matchRes: RegExpMatchArray | null = this.split('')
+        .sort()
+        .join('')
+        .match(/(.)\1*/g)
+      if (matchRes) {
+        return matchRes.reduce((res: ObjectAny, cur: string) => {
+          res[cur[0]] = cur.length
+          return res
+        }, {})
+      }
     }
+    return result
   }
-  return result
 }
 
 /**
@@ -145,39 +165,41 @@ String.prototype.getShowCounts = function (): object {
  * @param {Object} needle 替换值的映射对象
  * @returns {String}
  */
-String.prototype.batchReplace = function (needle: ObjectAny): string {
-  let match: string[] = Object.keys(needle)
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-  const escape: string[] = [
-    '\\',
-    '(',
-    ')',
-    '[',
-    ']',
-    '{',
-    '}',
-    '|',
-    '-',
-    '+',
-    '*',
-    '?',
-    '!',
-    '^',
-    '$',
-    '.'
-  ]
-  let i: string
-  for (i in match) {
-    let index: number = escape.indexOf(match[i])
-    if (index !== -1) {
-      match[i] = '\\' + escape[index]
+if (!String.prototype.hasOwnProperty('batchReplace')) {
+  String.prototype.batchReplace = function (needle: ObjectAny): string {
+    let match: string[] = Object.keys(needle)
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+    const escape: string[] = [
+      '\\',
+      '(',
+      ')',
+      '[',
+      ']',
+      '{',
+      '}',
+      '|',
+      '-',
+      '+',
+      '*',
+      '?',
+      '!',
+      '^',
+      '$',
+      '.'
+    ]
+    let i: string
+    for (i in match) {
+      let index: number = escape.indexOf(match[i])
+      if (index !== -1) {
+        match[i] = '\\' + escape[index]
+      }
     }
+    const rule: string = '(' + match.join('|') + ')'
+    const reg: RegExp = new RegExp(rule, 'g')
+    return this.replace(reg, function (_a: string, key) {
+      return needle[key]
+    })
   }
-  const rule: string = '(' + match.join('|') + ')'
-  const reg: RegExp = new RegExp(rule, 'g')
-  return this.replace(reg, function (_a: string, key) {
-    return needle[key]
-  })
 }
 
 /**
@@ -185,13 +207,15 @@ String.prototype.batchReplace = function (needle: ObjectAny): string {
  * @param {string[]} params 替换值
  * @returns {String}
  */
-String.prototype.sprintf = function (...params: string[]): String {
-  let str: String = this
-  //const args: IArguments = arguments
+if (!String.prototype.hasOwnProperty('sprintf')) {
+  String.prototype.sprintf = function (...params: string[]): String {
+    let str: String = this
+    //const args: IArguments = arguments
 
-  let i: string
-  for (i in params) {
-    str = str.replace(/%s/, params[i])
+    let i: string
+    for (i in params) {
+      str = str.replace(/%s/, params[i])
+    }
+    return str
   }
-  return str
 }
